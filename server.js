2eafa3ff1610eb99f88d6650c52081ade96c4c75
iegik <a.jansons@gmail.com>
2016-03-19 23:27:47 +0000

    Fix brackets-git

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var path = require('path'),
     http = require('http'),
     express = require('express'),
     brackets = require('brackets'),
-    fs = require('fs'),
-    state = require('./.brackets/state.json'),
     app = express(),
     server = http.createServer(app),
     bracketsOpts = {
@@ -19,21 +17,14 @@ var path = require('path'),
     var childProcess = require("child_process"),
         oldSpawn = childProcess.spawn;
     function mySpawn() {
-        fs.readFile('./.brackets/state.json', 'utf8', function (err, data) {
-            if (err) {
-                throw err;
-            }
-            state = JSON.parse(data);
-        });
-        state["brackets-git.currentGitRoot"] = state.projectPath;
-        fs.writeFile('./.brackets/state.json', JSON.stringify(state), 'utf8', function (err, data) {
-            if (err) {
-                throw err;
-            }
-            state = JSON.parse(data);
-        });
-        arguments[2] = {cwd:path.join(__dirname, '../', state.projectPath)};
-        console.log('cwd', arguments[2].cwd);
+        if(arguments[0] === '/usr/local/bin/git'){
+            arguments[2] = {
+                cwd:arguments[2].cwd
+                    .replace(/\\/g, '/')
+                    .replace(/^\/projects/,'../projects')
+                    .replace(/^\/support/,'.brackets')
+            };
+        }
         var result = oldSpawn.apply(null, arguments);
         return result;
     }
